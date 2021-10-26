0e239002233dc5925b25372a204bb0b0beffd6ae
AymanTheTinkerer <aymanismail2001@gmail.com>
2021-10-25 23:09:34 -0400

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Ayman Ismail on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
